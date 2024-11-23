762f46e52b06f5ba41878a0c6d3f59ad6401c8f6
juakoke <jamunoz30@uc.cl>
2024-11-23 20:52:23 -0300

    allowall cors

--- src/main.py
+++ src/main.py
@@ -11,10 +11,18 @@ from src.routers import (
     video,
 )
 import logging
+from fastapi.middleware.cors import CORSMiddleware
 
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(users.router)
