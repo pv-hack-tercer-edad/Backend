2ec45410a6301fd668acaf2acb7dc0e250ec6531
Marcelo Saldías <marcelo.saldias@inventures.cl>
2024-11-24 01:39:17 -0300

    fix: dont create a new transcription

--- src/routers/conversation_to_scenes.py
+++ src/routers/conversation_to_scenes.py
@@ -21,16 +21,12 @@ async def process_conversation(
     try:
         chapter = session.get(Chapter, chapter_id)
         logger.info(f"Starting conversation processing chapter:{chapter_id}")
-        new_transcription = Transcription(
-            text=chapter.transcription.content,
-            chapter_id=chapter_id,
-        )
         chapter.status = "PROCESSING"
-        session.add(new_transcription)
         session.commit()
         session.refresh(chapter)
-        print(new_transcription)
-        generate_scenes(new_transcription.content, new_transcription.id, session)
+        generate_scenes(
+            chapter.transcription.content, chapter.transcription.id, session
+        )
         await generate_video(chapter_id, session)
     except Exception as e:
         logger.error(f"Error processing conversation: {str(e)}")
